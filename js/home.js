1cdd6ed35312d14705e3e2358d182f2c9acd457d
nicoserena <nserenaolivera@udesa.edu.ar>
2020-05-29 17:07:28 -0300

    Con Nacho y Flor vinculamos la Home.html con la API de Deezer ; )

--- js/home.js
+++ js/home.js
@@ -9,22 +9,49 @@ window.addEventListener("load", function() {
     .then(function(information) {
         console.log(information);
     
-        let album = information.albums.data
+        let albums = information.albums.data
+        let artists = information.artists.data 
+        let tracks = information.tracks.data
 
-        console.log(album);
 
         for (let i = 0; i < 5; i++) {
-            const element = album[i];
+            const element = albums[i];
          
             let nombre = element.title
+            let imagen = element.cover
 
-            console.log(nombre);
-            /* hay que buscar la imagen del album y cambiarla en el html. 
-            Despues copipastear para tracks y artists */
+            document.querySelector("#fotos-albums").innerHTML += ` <li class="tops">
+            <a href="Albums.html"><img src="`+ imagen +`" alt="Foto de`+" " + nombre +` "> </a>    
+            </li>`
         }
+
         
+        for (let i = 0; i < 5; i++) {
+            const element = artists[i];
+         
+            let nombre = element.name
+            let imagen = element.picture
+
+            document.querySelector("#fotos-artists").innerHTML += ` <li class="tops">
+            <a href="Artists.html"><img src="`+ imagen +`" alt="Foto de`+" " + nombre +` "> </a>    
+            </li>`
+        }
+
+            
+        for (let i = 0; i < 5; i++) {
+            const element = tracks[i];
+        
+            let nombre = element.title
+            let imagen = element.album.cover
+
+            document.querySelector("#fotos-tracks").innerHTML += ` <li class="tops">
+            <a href="Tracks.html"><img src="`+ imagen +`" alt="Foto de cancion`+" " + nombre +` "> </a>    
+            </li>`
+        }
+
         
         
     })
+
 
 })
