a1d691c07c763711385448d4e8987201aa4b131f
nicoserena <nserenaolivera@udesa.edu.ar>
2020-06-16 17:38:30 -0300

    agregamos descripcion en la home

--- js/home.js
+++ js/home.js
@@ -9,51 +9,74 @@ window.addEventListener("load", function() {
     .then(function(information) {
         console.log(information);
     
-        let albums = information.albums.data
         let artists = information.artists.data 
         let tracks = information.tracks.data
+        let albums = information.albums.data
 
-
-        for (let i = 0; i < 5; i++) {
-            const element = albums[i];
-         
-            let nombre = element.title
-            let imagen = element.cover_xl
-
-            let idAlbum = element.id
-            
-            document.querySelector("#fotos-albums").innerHTML += ` <li class="tops">
-            <a href="Albums.html?idAlbum=`+ idAlbum +`"><img src="`+ imagen +`" alt="Foto de`+ " " + nombre +` "> </a>    
-            </li>`
-        }
-
-        
         for (let i = 0; i < 5; i++) {
             const element = artists[i];
          
-            let nombre = element.name
+            let nombreArtista = element.name
             let imagen = element.picture_xl
 
             let idArtist = element.id
             
             document.querySelector("#fotos-artists").innerHTML += ` <li class="tops">
-            <a href="Artists.html?idArtista=`+ idArtist +`"><img src="`+ imagen +`" alt="Foto de`+ " " + nombre +` "> </a>    
+            <a href="Artists.html?idArtista=`+ idArtist +`"><img src="`+ imagen +`" alt="Foto de`+ " " + nombreArtista +` ">
+            <h2 class="mobile">`+ nombreArtista +`</h2>
+            </a> 
             </li>`
         }
 
-            
+
         for (let i = 0; i < 5; i++) {
             const element = tracks[i];
         
-            let nombre = element.title
+            let nombreTrack = element.title
+            let nombreArtista = element.artist.name
             let imagen = element.album.cover_xl
 
             let idTrack = element.id
 
             document.querySelector("#fotos-tracks").innerHTML += ` <li class="tops">
-            <a href="Tracks.html?idTrack=`+ idTrack +`"><img src="`+ imagen +`" alt="Foto de cancion`+ " " + nombre +` "> </a>    
+            <a href="Tracks.html?idTrack=`+ idTrack +`"><img src="`+ imagen +`" alt="Foto de cancion`+ " " + nombreTrack +` ">
+            <h2 class="mobile">`+ nombreTrack +`</h2>
+            <h4 class="mobile1"> Song . `+ nombreArtista +`</h4>
+            </a>    
             </li>`
         }
+
+        /*let nombreTrack = tracks.title
+         document.querySelector(".tops").addEventListener("mouseover", function() {
+            document.querySelector.innerHTML = 
+            `
+            <h3 class="flow">` + nombreTrack + `</h3>
+            `
+            
+
+        })*/
+       
+        
+        for (let i = 0; i < 5; i++) {
+            const element = albums[i];
+         
+            let nombreAlbum = element.title
+            let nombreArtista = element.artist.name
+            let imagen = element.cover_xl
+            
+            let idAlbum = element.id
+            
+            document.querySelector("#fotos-albums").innerHTML += ` <li class="tops">
+            <a href="Albums.html?idAlbum=`+ idAlbum +`"><img src="`+ imagen +`" alt="Foto de`+ " " + nombreAlbum +` ">
+            <h2 class="mobile">`+ nombreAlbum +`</h2>
+            <h4 class="mobile1"> Album . `+ nombreArtista +`</h4>
+            </a>    
+            </li>`
+        }
+
+
+            
+
 
         
         
